1aea5a9bc656136a53550efd1a428955777f1577
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-09-09 17:39:34 +0700

    grouping response for product controller

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,16 +1,27 @@
 import { Exclude, Expose, Transform } from 'class-transformer';
 import { ObjectId } from 'mongoose';
 
+export const PRODUCT_SINGLE = 'product_single';
+export const PRODUCT_LIST = 'product_list';
+
 export class ProductEntity {
+
+    @Expose({ groups: [PRODUCT_SINGLE, PRODUCT_LIST] })
     name: string;
+
+    @Expose({ groups: [PRODUCT_SINGLE, PRODUCT_LIST] })
     price: number;
+
+    @Expose({ groups: [PRODUCT_SINGLE, PRODUCT_LIST] })
     category: string;
+
+    @Expose({ groups: [PRODUCT_SINGLE] })
     tags: string[];
 
     @Transform(({ value }) => value.toString())
     _id: ObjectId;
 
-    @Exclude()
+    @Expose({ groups: [PRODUCT_SINGLE] })
     description: string;
 
     constructor(partial: Partial<ProductEntity>) {
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, SerializeOptions, } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { FormatResponseInterceptor } from '../../common/interceptors/format-response.interceptors';
+import { PRODUCT_SINGLE, PRODUCT_LIST } from './entities/product.entity';
 
 @ApiBearerAuth()
 @ApiTags('Products')
@@ -14,21 +15,33 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @SerializeOptions({
+    groups: [PRODUCT_SINGLE]
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @SerializeOptions({
+    groups: [PRODUCT_LIST]
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @SerializeOptions({
+    groups: [PRODUCT_SINGLE]
+  })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @SerializeOptions({
+    groups: [PRODUCT_SINGLE]
+  })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
