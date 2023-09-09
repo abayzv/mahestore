ad6c612c9585b9c4e8d9fc2ea97758deb213b2a1
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-09-10 00:17:57 +0700

    refactor auth service code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,51 +6,57 @@ import { ResponseError } from 'src/common/error/error-exception';
 
 @Injectable()
 export class AuthService {
+    baseUrl: string = this.configService.get<string>('AUTH_SERVICE_URL');
+
     constructor(
         private readonly httpService: HttpService,
         private readonly configService: ConfigService,
     ) { }
 
     async login(email: string, password: string) {
-        const url = this.configService.get<string>('AUTH_SERVICE_URL') + '/auth/login';
-
-        const { data } = await firstValueFrom(
-            this.httpService.post(url, { email, password }).pipe(
-                catchError((error: any) => {
-                    throw new ResponseError(error.response.status, error.response.data.message);
-                })
-            )
-        )
+        const url = this.baseUrl + '/auth/login';
+        const data = await this.useApi(url, 'POST', { email, password })
 
         return data
     }
 
     async me(token: string) {
-        const url = this.configService.get<string>('AUTH_SERVICE_URL') + '/users/me';
-
-        const { data } = await firstValueFrom(
-            this.httpService.get(url, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }).pipe(
-                catchError((error: any) => {
-                    throw new ResponseError(error.response.status, error.response.data.message);
-                })
-            )
-        )
+        const url = this.baseUrl + '/users/me';
+        const data = await this.useApiAuth(url, 'GET', {}, token)
 
         return data.data
     }
 
     async verify(token: string, action: string, path: string) {
-        const url = this.configService.get<string>('AUTH_SERVICE_URL') + '/permissions/verify';
+        const url = this.baseUrl + '/permissions/verify';
+        const data = await this.useApiAuth(url, 'POST', { action, path }, token)
 
-        const { data } = await firstValueFrom(
-            this.httpService.post(url, {
-                action,
-                path
-            }, {
+        return data
+    }
+
+    private async useApi(url: string, method: string, data: any) {
+        const { data: response } = await firstValueFrom(
+            this.httpService.request({
+                url,
+                method,
+                data,
+            }).pipe(
+                catchError((error: any) => {
+                    throw new ResponseError(error.response.status, error.response.data.message);
+                })
+            )
+        )
+
+        return response
+
+    }
+
+    private async useApiAuth(url: string, method: string, data: any, token: string) {
+        const { data: response } = await firstValueFrom(
+            this.httpService.request({
+                url,
+                method,
+                data,
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -61,7 +67,8 @@ export class AuthService {
             )
         )
 
-        return data
+        return response
+
     }
 
 }
